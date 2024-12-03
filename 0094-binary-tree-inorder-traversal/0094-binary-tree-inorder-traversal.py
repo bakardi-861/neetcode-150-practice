5ab5491f0acac1a05289579f7540d5d2ad24e7d3
bakardi-861 <58563075+bakardi-861@users.noreply.github.com>
2024-12-03 15:16:15 -0600

    Time: 0 ms (100%), Space: 17.5 MB (5.02%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -9,12 +9,13 @@ class Solution:
         # go as deep left as we can, return when we hit null, pop back up
         # then return the root
         # then go as deep right as we can, return when null
+
+        # see if there's a way to do this all in one function
         def inorder(root,res):
-            if not root:
-                return None
-            inorder(root.left, res)
-            res.append(root.val)
-            inorder(root.right, res)
+            if root:
+                inorder(root.left, res)
+                res.append(root.val)
+                inorder(root.right, res)
 
         res = []
         inorder(root, res)
