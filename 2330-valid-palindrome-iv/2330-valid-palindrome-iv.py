26f0eea36c3346cdf3444c45d13fdce360d3abae
bakardi-861 <58563075+bakardi-861@users.noreply.github.com>
2025-02-09 20:05:18 -0600

    Time: 3 ms (78.98%), Space: 18 MB (40.09%) - LeetHub

--- 2330-valid-palindrome-iv/2330-valid-palindrome-iv.py
+++ 2330-valid-palindrome-iv/2330-valid-palindrome-iv.py
@@ -1,6 +1,7 @@
 class Solution:
     def makePalindrome(self, s: str) -> bool:
         # when we have a mismatch, count ops. if count exceeds 2, return false.
+        # be able to explain why this works
         l,r = 0,len(s)-1
         count = 0
         while l <= r:
