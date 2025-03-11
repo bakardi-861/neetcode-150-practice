b1dad90f0b4410a85e9fb722a2f9a8507901fae5
bakardi-861 <58563075+bakardi-861@users.noreply.github.com>
2025-03-11 10:30:23 -0500

    Time: 33 ms (5.02%), Space: 19.3 MB (14.55%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,8 +1,44 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        # same as majority element 1, but just seeing if it appears n//3 times?
-        # is B.M. only useful when we have the n // 2 condition?
+        def updateCount(counts, candidates,n,k):
+            for i in range(k):
+                if candidates[i] != None and candidates[i] == n:
+                    counts[i] += 1
+                    return True
+            return False
+
+        def populateCandidate(counts,candidates,n,k):
+            if (updateCount(counts,candidates,n,k)):
+                return
+
+            for i in range(k):
+                if counts[i] == 0:
+                    candidates[i] = n
+                    counts[i] += 1
+                    return
+
+            for i in range(k):
+                counts[i] -= 1
+
+
+        k = 3
+        res = []
+        if len(nums) <= k-1:
+            return list(set(nums))
         
-        counter = Counter(nums)
-        return [c for c in counter if counter[c] > len(nums)//3]
+        counts = [0] * (k-1)
+        candidates = [None] * (k-1)
+
+        for n in nums:
+            populateCandidate(counts,candidates,n,k-1)
+        
+        counts = [0] * (k-1)
+        for n in nums:
+            updateCount(counts,candidates,n,k-1)
+        
+        threshold = len(nums)/k
+        for i in range(k-1):
+            if counts[i] > threshold:
+                res.append(candidates[i])
+        return res
 
