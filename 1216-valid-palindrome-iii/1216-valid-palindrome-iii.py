9ba240d0e3ed8d899d1d9da8f813553c33297439
bakardi-861 <58563075+bakardi-861@users.noreply.github.com>
2025-03-16 09:52:58 -0500

    Time: 1563 ms (13.01%), Space: 257.7 MB (31.92%) - LeetHub

--- 1216-valid-palindrome-iii/1216-valid-palindrome-iii.py
+++ 1216-valid-palindrome-iii/1216-valid-palindrome-iii.py
@@ -2,17 +2,18 @@ class Solution:
     def isValidPalindrome(self, s: str, k: int) -> bool:
         memo = {}
         def isValid(l,r,k):
+            # RECURSION + MEMO
             if (l,r,k) in memo:
                 return memo[(l,r,k)]
 
-            while l <= r: 
+            while l < r: # bc we need 2 characters to choose to remove
                 if s[l] != s[r]:
-                    if not k:
-                        memo[(l,r,k)] = False
-                    else:
+                    if k > 0:
+                        # choose one character to remove on either side
                         memo[(l,r,k)] = isValid(l+1,r,k-1) or isValid(l,r-1,k-1)
+                    else: # k == 0, so can't remove anymore characters
+                        memo[(l,r,k)] = False
                     return memo[(l,r,k)]
-                
                 l += 1
                 r -= 1
             return True
