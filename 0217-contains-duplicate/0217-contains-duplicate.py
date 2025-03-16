ad3a8291bef2c6cf2af697f6a730cf9c9a66b8b1
bakardi-861 <58563075+bakardi-861@users.noreply.github.com>
2025-03-16 07:10:08 -0500

    Time: 47 ms (6.78%), Space: 26.8 MB (93.14%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,12 +1,19 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        # sort and 2 pointers - O(nlogn) time, O(1) space
+        # sort and for loop - O(nlogn) time, O(1) space
         # set - O(n) time and space
 
-        #set 
-        dupes = set()
-        for n in nums:
-            if n in dupes:
+        #sort
+        nums.sort()
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
                 return True
-            dupes.add(n)
         return False
+
+        #set 
+        # dupes = set()
+        # for n in nums:
+        #     if n in dupes:
+        #         return True
+        #     dupes.add(n)
+        # return False
