cf765fbe49edfb785d13e68b7684feb3e51fd150
bakardi-861 <58563075+bakardi-861@users.noreply.github.com>
2025-03-17 13:23:48 -0500

    Time: 0 ms (100%), Space: 19 MB (34.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # hashmap - store diff of target and current num. 
-        # return True when we encounter that difference as a number in nums.
         hashmap = {}
-
         for i in range(len(nums)):
             diff = target - nums[i]
             if diff in hashmap:
                 return [hashmap[diff],i]
             hashmap[nums[i]] = i
-        # return [-1,-1]
