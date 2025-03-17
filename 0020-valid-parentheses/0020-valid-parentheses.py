7a9eb779c41a8080d1998b3042f2bf579bd165a1
bakardi-861 <58563075+bakardi-861@users.noreply.github.com>
2025-03-17 17:26:56 -0500

    Time: 0 ms (100%), Space: 17.9 MB (51.17%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,19 +1,13 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # 1. open and closed types must match
-        # 2. open and closed must be closed in order
-        # 3. every closed has an open bracket of the same type
-
-        map = {")":"(", "}":"{","]":"["}
-        # s = "(([])){}))"
-        # c = ")"
         stack = []
-
-        for c in s:
-            if stack and c in map: # c is closed and stack has something on it
-                if stack[-1] != map[c]: # if type of brackets DO NOT match
+        brackets = {")":"(","}":"{","]":"["}
+        for i,c in enumerate(s):
+            if c in "]})":
+                if stack and brackets[c] == stack[-1]:
+                    stack.pop()
+                    continue
+                else:
                     return False
-                stack.pop()
-                continue # continue to next iteration
-            stack.append(c) # append when open
+            stack.append(c)
         return len(stack) == 0
