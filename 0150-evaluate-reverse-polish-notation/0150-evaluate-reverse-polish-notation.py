53a352e47a3f55c02fef05bb8c2bd5c7416bab13
bakardi-861 <58563075+bakardi-861@users.noreply.github.com>
2025-03-18 09:52:11 -0500

    Time: 4 ms (29.88%), Space: 19.2 MB (52.5%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -0,0 +1,21 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        # RPN = PEMDAS??
+        # round decimals down
+        stack = []
+        for t in tokens:
+            if stack and t in "+/*-":
+                num2 = stack.pop()
+                num1 = stack.pop()
+                if t == "+":
+                    math = num1 + num2
+                elif t == "-":
+                    math = num1 - num2
+                elif t == "*":
+                    math = num1 * num2
+                elif t == "/":
+                    math = int(num1 / num2) # int() truncates towards 0
+                stack.append(math)
+            else:
+                stack.append(int(t))
+        return stack[0]
