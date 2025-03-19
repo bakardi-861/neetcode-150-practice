74882b68eaed3e98d5f9093f17e7f354fd05d6ef
bakardi-861 <58563075+bakardi-861@users.noreply.github.com>
2025-03-19 14:23:46 -0500

    Time: 40 ms (39.22%), Space: 17.5 MB (99.44%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.py
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.py
@@ -10,12 +10,10 @@ class Solution:
         # binary search range 1 - n
 
         l,r = 1,n
-        while l <= r:
+        while l < r:
             mid = (l+r)//2
-            if guess(mid) == -1:
-                r = mid - 1
+            if guess(mid) <= 0:
+                r = mid
             elif guess(mid) == 1:
                 l = mid + 1
-            else:
-                return mid
-        return -1
+        return l
