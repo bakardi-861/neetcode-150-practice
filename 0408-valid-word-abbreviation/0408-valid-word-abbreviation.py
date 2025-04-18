2d1e6c4ff3ac2b33486abd65707951ba1f29051e
bakardi-861 <58563075+bakardi-861@users.noreply.github.com>
2025-04-18 05:29:04 -0500

    Time: 0 ms (100%), Space: 17.5 MB (99.91%) - LeetHub

--- 0408-valid-word-abbreviation/0408-valid-word-abbreviation.py
+++ 0408-valid-word-abbreviation/0408-valid-word-abbreviation.py
@@ -1,35 +1,21 @@
 class Solution:
     def validWordAbbreviation(self, word: str, abbr: str) -> bool:
-        # substitution    abbr: s10n
-        #            i             j
-        # internationalization
-        #              i
         i = j = 0
         while i < len(word) and j < len(abbr):
             # build the number
             if abbr[j].isnumeric():
-                if abbr[j] == "0":
+                if abbr[j] == "0":# can't have leading 0s
                     return False
+                
                 number = ""
-                while j < len(abbr) and abbr[j].isnumeric(): # can't have leading 0s
+                while j < len(abbr) and abbr[j].isnumeric():
                     number += abbr[j]
                     j += 1
                 i += int(number)
-                
-                # # get each digit of the number string and 
-                # num = 0
-                # for n in reversed(number):
-                #     num += (int(n) % 10)
 
-            else: # both are letters, so skip ahead
+            else: # both are letters, so skip ahead if letters are the same
                 if abbr[j] != word[i]: # letters are not the same
                     return False
                 j += 1
                 i += 1
         return i == len(word) and j == len(abbr)
-
-
-         # skip letters in the abbreviation/word
-         # when abbr[j] is a number, loop through it to build the number from the characters until we reach another letter.
-         # after building the number, move i ahead that many characters. i should be on the same letter j is at.
-         # if abbreviation is valid, i == len(word) and j == len(abbr) once algo exits, else it is not valid.
