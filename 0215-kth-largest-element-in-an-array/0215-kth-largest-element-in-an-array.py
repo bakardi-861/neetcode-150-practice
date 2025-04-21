d2ac411b4a21e1e235f357f95681be38536cf16f
bakardi-861 <58563075+bakardi-861@users.noreply.github.com>
2025-04-20 19:55:46 -0500

    Time: 66 ms (68.39%), Space: 28.9 MB (26.27%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,13 +1,10 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        # heap is O(n) time, O(k) space
-        # quickselect is O(n) time, O(1) space
-        # I know I can't remember it all, but see how much I can get
-        k = len(nums)-k # needs to do this because quickselect finds the kth smallest, so kth largest would be "n-k" smallest
-                        # In Example 1: 2nd largest element is the 4th smallest.
+        k = len(nums)-k
         l,r = 0,len(nums)-1
-        while l <= r: # needs to be <=
-            pivot = nums[(random.randint(l,r))]
+
+        while l <= r:
+            pivot = nums[random.randint(l,r)]
             lt,gt,i = l,r,l
 
             while i <= gt:
@@ -15,16 +12,17 @@ class Solution:
                     nums[lt],nums[i] = nums[i],nums[lt]
                     lt += 1
                     i += 1
-                elif nums[i] > pivot: 
+                elif nums[i] > pivot:
                     nums[gt],nums[i] = nums[i],nums[gt]
                     gt -= 1
                 else:
                     i += 1
-
-            # comlpetely forgot this part
+            
             if k < lt:
                 r = lt - 1
             elif k > gt:
                 l = gt + 1
             else:
                 return nums[k]
+        return -1
+
