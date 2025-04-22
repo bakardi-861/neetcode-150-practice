284ae1bd189f1376c0f58fb2b5c2fa1a1f4f0986
bakardi-861 <58563075+bakardi-861@users.noreply.github.com>
2025-04-21 23:14:20 -0500

    Time: 37 ms (65.61%), Space: 40.2 MB (71.81%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.py
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.py
@@ -7,26 +7,19 @@ class Solution:
     def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         # naive: copy list, reverse, and get kth node from the end (4) and swap with kth node from beginning in original list (2).
         # fast/slow, move fast k steps ahead. move fast/slow one at a time until fast.next is null.
-        # slow will be at kth from the end
-        # when i == k, swap values, break and return??
-
-        # [1,2,3,4,5]
-        #    h
-        #        s
 
         dummy = ListNode(0)
         dummy.next = head
 
-        fast = slow = head
+        start = end = head
         for _ in range(k-1):
-            fast = fast.next
-
-        while fast and fast.next:
-            fast = fast.next
-            slow = slow.next
+            start = start.next
+# only need to traverse the list once so start curr at start.next and when it reaches the end, "end" will be the kth from the end.
+# this is because we moved start k steps ahead
+        curr = start.next 
+        while curr:
+            end = end.next
+            curr = curr.next
         
-        for i in range(k):
-            if i == k-1:
-                head.val,slow.val = slow.val,head.val
-            head = head.next
+        start.val,end.val = end.val,start.val
         return dummy.next
