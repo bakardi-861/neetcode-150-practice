c132a5efdbeb3e0c3a59c13e212652705aa84ee5
bakardi-861 <58563075+bakardi-861@users.noreply.github.com>
2025-04-21 22:52:54 -0500

    Time: 84 ms (44.46%), Space: 41.4 MB (80.82%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
@@ -5,35 +5,43 @@
 #         self.next = next
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
-        # in one pass we can use a stack to store each node and we see its twin, calculate the sum. 
-        # pop node from stack.
-        # if it's the max so far, update the max.
+        # fast/slow, reverse second half and then get sum by adding alternating values
+        # this works bc n is always even
+        
+        def reverse(head):
+            prev,curr = None,head
+            while curr:
+                curr.next,prev,curr = prev,curr,curr.next
+            return prev
 
         dummy = ListNode(0)
         dummy.next = head
 
-        # get n (size of list)
-        n = 0
-        c = head
-        while c:
-            n += 1
-            c = c.next
+        fast = slow = head
+        prev = dummy
 
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
 
-        stack = []
+        prev.next = None
+
+        second = reverse(slow)
+        first = head
         max_sum = 0
-        i = 0
-        curr = head
+        first_sum = 0
+        second_sum = 0
+
+        # head = [5,4,2,1]
+        # first = [4]
+        # second = [2]
+        # sum = 6
+
         
-        while curr and i < n:
-            if stack and stack[-1][1] == n-1-i:
-                sum = stack[-1][0] + curr.val
-                max_sum = max(sum,max_sum)
-                stack.pop()
-            else:
-                stack.append([curr.val,i])
-           
-            i += 1
-            curr = curr.next
-        
+        while first and second: 
+            sum = first.val + second.val
+            max_sum = max(max_sum, sum)
+            first = first.next
+            second = second.next
         return max_sum
