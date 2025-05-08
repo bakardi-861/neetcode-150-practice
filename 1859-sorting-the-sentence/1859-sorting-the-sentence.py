ca27d70e07055457a123ec7acf382f552afaae57
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-05-08 09:16:26 -0500

    Time: 39 ms (47.4%), Space: 17.6 MB (92.21%) - LeetHub

---
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -0,0 +1,18 @@
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        # divide each word into into an array of strings
+        # bucket sort bc number of letters is 1-9?
+        # digit is always the last character of a word
+        arr = s.split()
+
+        # if not arr:
+        #     return arr
+
+        n = len(arr)
+        buckets = ["" for _ in range(n)]
+
+        # Step 1: Distribute elements into buckets
+        for word in arr:
+            digit = word[-1] # get digit at end of word
+            buckets[int(digit)-1] = word[0:len(word)-1]
+        return " ".join(buckets)
