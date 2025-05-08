f35e7af4d5f7bedbd0bbb03aaa40d9e5e963c3a4
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-05-08 09:42:33 -0500

    Time: 38 ms (54.41%), Space: 17.9 MB (28.63%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.py
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -1,36 +1,7 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
         arr = s.split()
-        l, r = 0, len(arr) - 1
-
-        def index(word):
-            return int(word[-1]) - 1
-
-        # In-place QuickSort with 3-way partitioning
-        def quicksort(l, r):
-            if l >= r:
-                return
-
-            pivot_idx = random.randint(l, r)
-            pivot = index(arr[pivot_idx])
-            lt, gt, i = l, r, l
-
-            while i <= gt:
-                current = index(arr[i])
-                if current < pivot:
-                    arr[lt], arr[i] = arr[i], arr[lt]
-                    lt += 1
-                    i += 1
-                elif current > pivot:
-                    arr[gt], arr[i] = arr[i], arr[gt]
-                    gt -= 1
-                else:
-                    i += 1
-
-            quicksort(l, lt - 1)
-            quicksort(gt + 1, r)
-
-        quicksort(l, r)
-
-        # Remove the trailing digit and join
-        return " ".join(word[:-1] for word in arr)
+        arr.sort(key=lambda w: int(w[-1]))
+        for i in range(len(arr)):
+            arr[i] = arr[i][:-1]
+        return " ".join(arr)
