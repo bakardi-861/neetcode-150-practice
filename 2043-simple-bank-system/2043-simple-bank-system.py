e0714d95a20623cce7fbb3df08a942058c2f4e47
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-06-15 11:58:55 -0500

    Time: 28 ms (74.93%), Space: 47.8 MB (48.76%) - LeetHub

--- 2043-simple-bank-system/2043-simple-bank-system.py
+++ 2043-simple-bank-system/2043-simple-bank-system.py
@@ -3,36 +3,37 @@ class Bank:
     # 1. account number is from 1 to n (len(balance))
     # 2. the amount of money withdrawn/transferred <= account balance
 
+    # Time: O(n) for creating accounts map and O(1) for everything else
+    # Space: O(n) store the accounts
+
     def __init__(self, balance: List[int]):
         # create hashmap of accounts and insert balances as {i:balance[i]}
         # if i already in accounts, don't insert anything
-        self.accounts = {}
-        for i in range(len(balance)):
-            if i+1 not in self.accounts:
-                self.accounts[i+1] = balance[i]
+        self.balance = balance
+        self.n = len(balance)
         #print(self.accounts)
         
     def transfer(self, account1: int, account2: int, money: int) -> bool:
         # if either account is not in accounts, return False.
-        if account1 not in self.accounts or account2 not in self.accounts or money > self.accounts[account1]:
+        if account1-1 > self.n or account2-1 > self.n or money > self.balance[account1-1]:
             return False
         
-        self.accounts[account1] -= money
-        self.accounts[account2] += money
+        self.balance[account1-1] -= money
+        self.balance[account2-1] += money
         #print(self.accounts)
         return True
 
     def deposit(self, account: int, money: int) -> bool:
-        if account not in self.accounts:
+        if account > self.n:
             return False
-        self.accounts[account] += money
+        self.balance[account-1] += money
         #print(self.accounts)
         return True
 
     def withdraw(self, account: int, money: int) -> bool:
-        if account not in self.accounts or money > self.accounts[account]:
+        if account > self.n or money > self.balance[account-1]:
             return False
-        self.accounts[account] -= money
+        self.balance[account-1] -= money
         #print(self.accounts)
         return True
 
