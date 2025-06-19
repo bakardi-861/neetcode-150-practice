130715453967aede975d0fd378acb9a29ccd5817
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-06-19 10:06:39 -0500

    Time: 39 ms (77.05%), Space: 28.1 MB (79.82%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
@@ -1,27 +1,19 @@
 class Solution:
-    def numOfSubarrays(self, nums: List[int], k: int, threshold: int) -> int:
-        # nums = [2,2,2,2,5,5,5,8], k = 3, threshold = 4
-        #                    l
-        #                      r
-        # avg = sum / (r-l+1) = 6 res = 3
-        
-        # maintain fixed size window of size k
-        # early terminate when len(win)-l < k
-        res = 0
-        l = 0
-        win_sum = 0
-        for r in range(len(nums)):
-            win_sum += nums[r]
+    def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
+        #  0   1. 2. 3. 4 5  6 7 8 9
+        # [11,13,17,23,29,31,7,5,2,3]
+        #  l
+        #  r
+        #            k
+        # k = 3, threshold = 5
 
-            # if too big, shrink it
-            if r-l+1 > k:
-                win_sum -= nums[l]
-                l += 1
-            
-            # if avg >= threshold and window size == k
-            avg = win_sum / (r-l+1)
-            if avg >= threshold and r-l+1 == k:
-                res += 1
-        return res
-            
+        ans = 0
+        win_sum = sum(arr[:k])
+        if win_sum >= threshold * k:
+            ans += 1
 
+        for i in range(k, len(arr)):
+            win_sum += arr[i] - arr[i - k]
+            if win_sum >= threshold * k:
+                ans += 1
+        return ans
