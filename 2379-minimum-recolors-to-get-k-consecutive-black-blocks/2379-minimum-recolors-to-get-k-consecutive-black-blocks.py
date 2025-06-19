10af7559d787f8ac9957b9be6262baf5718ff5d5
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-06-19 10:13:31 -0500

    Time: 2 ms (32.71%), Space: 17.9 MB (37.43%) - LeetHub

--- 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.py
+++ 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.py
@@ -1,18 +1,9 @@
-#from collections import defaultdict
 class Solution:
     def minimumRecolors(self, blocks: str, k: int) -> int:
-        min_ops = float("inf")
-        l,r = 0,0
-        win = defaultdict(int)
-        for r in range(len(blocks)):
-            win[blocks[r]] += 1
-            # if win too big, shrink it
-            if r-l+1 > k:
-                win[blocks[l]] -= 1
-                if win[blocks[l]] == 0: del win[blocks[l]]
-                l += 1
-
-            # if window size is k, get the min ops needed
-            if r-l+1 == k:
-                min_ops = min(win["W"],min_ops)
+        win = Counter(blocks[:k])
+        min_ops = min(float("inf"),win["W"])
+        for i in range(k,len(blocks)):
+            win[blocks[i]] += 1
+            win[blocks[i-k]] -= 1
+            min_ops = min(min_ops,win["W"])
         return min_ops
