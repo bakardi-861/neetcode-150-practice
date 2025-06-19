b783f45e54382144e0d4879d7663c7b06f004388
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-06-19 09:18:07 -0500

    Time: 53 ms (14.34%), Space: 30.5 MB (88.06%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,23 +1,17 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        sliding = set()
-
-        # if k = 0, then False because there can be no answer
         if k == 0:
             return False
 
-        # add numbers to set up to k
-        for c in nums[:k]:
-            if c in sliding: # if we found a duplicate, then True
+        l,r = 0,0
+        win = set()
+        while r < len(nums):
+            if nums[r] in win:
                 return True
-            sliding.add(c)
 
-        # 
-        for i in range(k, len(nums)):
-            if nums[i] in sliding:
-                return True
-            # remove first item (l) from set if window too big
-            sliding.discard(nums[i-k]) # does not raise an error if num not in set like .remove() does
-            sliding.add(nums[i]) # add current to sliding window
-
+            win.add(nums[r])
+            r += 1
+            if abs(r-l) > k:
+                win.discard(nums[l])
+                l += 1
         return False
