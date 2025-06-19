c038b5e5f6395963fc337bf75c07d036ffe07e31
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-06-19 15:49:24 -0500

    Time: 163 ms (36.53%), Space: 17.9 MB (86.58%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -15,14 +15,10 @@ class Solution:
         max_f = -float("inf")
         for r in range(len(s)):
             win[s[r]] += 1
-            max_f = max(win.values())
-            min_f = (r-l+1) - max_f
-            while min_f > k:
+            max_f = max(max_f,win[s[r]])
+            while (r-l+1) - max_f > k:
                 win[s[l]] -= 1
                 l += 1
-                # update min if changed
-                min_f = (r-l+1) - max(win.values())
-            if min_f <= k:
-                longest = max(longest,r-l+1)
+            longest = max(longest,r-l+1)
         return longest
 
