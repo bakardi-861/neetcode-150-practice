9afcffbf0f26dd6872dea270ddc50391da32599e
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-07-14 09:15:56 -0500

    Time: 367 ms (10.18%), Space: 17.9 MB (63.75%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
@@ -1,28 +1,16 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        #  0123456
-        # "abcabc"
-        #       r
-        #     l
-        # win = {a:1,b:1,c:1}
-        # # if win == chars # keys == "abc"
-        #     ans += 1 + ((len(s)-1) - r)
-        #     ans = 10
+        count = defaultdict(int)
+        res = 0
+        left = 0
 
-        
-        win = {"a":0,"b":0,"c":0}
-        l = 0
-        ans = 0
-        chars = set(["a","b","c"])
-        for r in range(len(s)):
-            win[s[r]] += 1
-            while self.has_all_chars(win):
-                ans += 1 + ((len(s)-1) - r)
-                win[s[l]] -= 1
-                l += 1
-        return ans
+        for right in range(len(s)):
+            count[s[right]] += 1
 
-    def has_all_chars(self,win:dict):
-        return all(f > 0 for f in win.values())
+            # shrink window from left while valid
+            while all(count[c] > 0 for c in 'abc'):
+                res += len(s) - right
+                count[s[left]] -= 1
+                left += 1
 
-
+        return res
