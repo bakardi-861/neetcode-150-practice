719453845631769c605001169975172db417c1d2
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-08-25 19:43:40 -0400

    Time: 7 ms (98%), Space: 21.3 MB (43.37%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,7 +1,20 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # group sorted letter combinations together in a hashmap
-        s_map = defaultdict(list)
+        # group by characters and counts of each char
+        #strs = ["eat","tea","tan","ate","nat","bat"]                                i
+        # {(e:1,a:1,t:1):["eat","tea","ate"],(t:1,a:1,n:1):["tan","nat"], (b:1,a:1,t:1):["bat"]}
+            # [["eat","tea","ate"],["tan","nat"],["bat"]]
+        ana_map = defaultdict(List[str])
+
         for s in strs:
-            s_map[tuple(sorted(s))].append(s)
-        return [s_map[s] for s in s_map]
+            s_count = tuple(sorted(s)) #O(nlogk)
+            if s_count in ana_map:
+                ana_map[s_count].append(s)
+            else:
+                ana_map[s_count] = [s]
+        
+        res = []
+        for kv in ana_map:
+            res.append(ana_map[kv])
+        return res
+
