d37f9cc65f8ba285daa3e38140ed6a99b218d970
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-08-26 19:16:34 -0400

    Time: 7 ms (81.38%), Space: 17.9 MB (95.16%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -6,14 +6,14 @@ class Solution:
         #                   r
 
         l,r = 0,len(s)-1
-        while l <= r:
+        while l < r:
 
-            while l <= r and not s[l].isalnum():
+            while l < r and not s[l].isalnum():
                 l += 1
-            while l <= r and not s[r].isalnum():
+            while l < r and not s[r].isalnum():
                 r -= 1
 
-            if l <= r and s[l].lower() != s[r].lower():
+            if s[l].lower() != s[r].lower():
                 return False
             l += 1
             r -= 1
