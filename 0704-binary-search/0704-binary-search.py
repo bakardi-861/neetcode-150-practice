998a89e8236f6dd9bc87cc4fe758cdae71db5842
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-08-26 19:30:31 -0400

    Time: 0 ms (100%), Space: 18.7 MB (45.19%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,17 +1,14 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # return first and last instance of target
+        #   0.1 2 3 4 5. 6
+        # [-1,0,3,5,9,12]
+        #  l      m      r
+        
         l,r = 0,len(nums)-1
         while l < r:
-            mid = (r+l)//2 # need + 1 when finding last occurence to have a right bias
-
-            if nums[mid] < target: # target is larger than mid, or == mid
+            mid = (r+l) // 2
+            if nums[mid] < target:
                 l = mid + 1
-            else: # target is smaller than mid, shrink space
-               r = mid
+            elif nums[mid] >= target:
+                r = mid
         return l if nums[l] == target else -1
-        #   0 1 2 3 4 5 
-        # [-1,0,3,5,9,12].  target = 9
-        #           m
-        #           l
-        #                 r
