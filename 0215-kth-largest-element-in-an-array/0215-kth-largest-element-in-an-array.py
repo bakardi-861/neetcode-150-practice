9f78d06a934f10689d8118a78fa8ef2519cdf5b6
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-09-02 13:51:38 -0400

    Time: 77 ms (65.38%), Space: 28.7 MB (69.62%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -6,9 +6,34 @@ class Solution:
         # [3,2,1,5,6,4] -> [1,2,3,4,5,6]
 
         #heap
-        heap = nums
-        heapify(heap)
+        # heap = nums
+        # heapify(heap)
 
-        while len(heap) > k:
-            heappop(heap)
-        return heap[0] if heap else -1
+        # while len(heap) > k:
+        #     heappop(heap)
+        # return heap[0] if heap else -1
+
+        # quickselect
+        k = len(nums) - k
+        l,r = 0,len(nums)-1
+        while l <= r:
+            lt,gt,i = l,r,l
+            pivot = nums[random.randint(l,r)]
+
+            while i <= gt:
+                if nums[i] < pivot:
+                    nums[lt],nums[i] = nums[i],nums[lt]
+                    lt += 1
+                    i += 1
+                elif nums[i] > pivot:
+                    nums[gt],nums[i] = nums[i],nums[gt]
+                    gt -= 1
+                else:
+                    i += 1
+            
+            if k < lt:
+                r = lt - 1
+            elif k > gt:
+                l = gt + 1
+            else:
+                return nums[k]
