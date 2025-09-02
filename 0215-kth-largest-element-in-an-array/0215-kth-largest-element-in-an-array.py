39a1a0e0d3c99966ff6b192cfff2c01d2e0042df
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-09-02 13:48:35 -0400

    Time: 88 ms (58.18%), Space: 27.6 MB (95.49%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,26 +1,14 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        k = len(nums) - k
-        l,r = 0,len(nums)-1
-        while l <= r:
-            pivot = nums[random.randint(l,r)]
-            lt,gt,i = l,r,l
+        # quickselect O(n^2) in worse case, O(n) best, O(1) space
+        # heap = O(nlogn), O(n) space
 
-            while i <= gt:
-                if nums[i] < pivot:
-                    nums[i],nums[lt] = nums[lt],nums[i]
-                    lt += 1
-                    i += 1
-                elif nums[i] > pivot:
-                    nums[i],nums[gt] = nums[gt], nums[i]
-                    gt -= 1
-                else:
-                    i += 1
-            
-            if k < lt:
-                r = lt - 1
-            elif k > gt:
-                l = gt + 1
-            else:
-                return nums[k]
-        return -1
+        # [3,2,1,5,6,4] -> [1,2,3,4,5,6]
+
+        #heap
+        heap = nums
+        heapify(heap)
+
+        while len(heap) > k:
+            heappop(heap)
+        return heap[0] if heap else -1
