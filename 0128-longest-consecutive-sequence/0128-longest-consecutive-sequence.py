2c00757b7ba96367beccb8046cda87a0e71fb824
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-09-14 11:28:29 -0400

    Time: 343 ms (5.04%), Space: 44.6 MB (5.05%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -2,13 +2,33 @@ class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         # hash table: track sequence starts, if num-1 not in set
         # union find: connected component = consecutive sequence (num + 0 or num + 1 or num - 1)
-        num_set = set(nums)
-        max_len = 0
+        parent = {}
+        size = {}
 
-        for num in num_set:
-            if num - 1 not in num_set:
-                length = 1
-                while num + length in num_set:
-                    length += 1
-                max_len = max(max_len, length)
-        return max_len
+        def find(x):
+            while x != parent[x]:
+                parent[x] = parent[parent[x]]  # path compression
+                x = parent[x]
+            return x
+
+        def union(x, y):
+            rootX, rootY = find(x), find(y)
+            if rootX == rootY: return
+            if size[rootX] < size[rootY]:
+                rootX, rootY = rootY, rootX
+            parent[rootY] = rootX
+            size[rootX] += size[rootY]
+
+        # initialize nodes
+        for num in nums:
+            parent[num] = num
+            size[num] = 1
+
+        # union consecutive neighbors
+        for num in nums:
+            if num - 1 in parent:
+                union(num, num - 1)
+            if num + 1 in parent:
+                union(num, num + 1)
+
+        return max(size.values(), default=0)
