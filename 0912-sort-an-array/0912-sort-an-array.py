2cb2964a25ce43a09933361531e5ec26d23de12c
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-09-22 10:05:35 -0400

    Time: 111 ms (84.04%), Space: 24.9 MB (55.72%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,37 +1,11 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        #      l
-        #        r
-        # [1,2,5,9]
-        #        p
-        #      lt
-        #        g 
-        #        i
+        # learn merge sort
+        # try heap sort
+        # try counting/bucket sort
+        # learn radix sort
 
-        def quicksort(l,r): # indices are inclusive
-            if r-l+1 == 0 or r-l+1 == 1:
-                return
-            pivot = nums[random.randint(l,r)]
-            lt, gt, i = l,r,l
-            
-            while i <= gt:
-                if nums[i] < pivot:
-                    nums[lt],nums[i] = nums[i],nums[lt]
-                    lt += 1
-                    i += 1
-                elif nums[i] > pivot:
-                    nums[gt],nums[i] = nums[i],nums[gt]
-                    gt -= 1
-                else: # i == pivot
-                    i += 1
-            
-            quicksort(l,lt-1)
-            quicksort(gt+1,r)
-
-        quicksort(0,len(nums)-1)
-        return nums
-
-    
-
-
-        
+        # heap sort
+        heapify(nums)
+        sorted_nums = [heappop(nums) for i in range(len(nums))]
+        return sorted_nums
