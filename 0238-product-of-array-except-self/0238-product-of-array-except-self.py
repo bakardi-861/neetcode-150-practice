4b2ca5ec1429374c9dce735130d61cd92baed602
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-09-26 12:42:47 -0400

    Time: 21 ms (69.28%), Space: 23.4 MB (54.21%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,16 +1,57 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-         # prefix sums?
+        #         0 1 2 3
+        # nums = [1,2,3,4]
+        # ans =  [24,12,8,6]
+        # Output: [24,12,8,6]
+        #           0 1 2 3
+        # # nums = [1,2,3,4]
+        #             i
+        #             l
+        #               r
+        # # prefix products = [1,1,2,6,24]
+        #                          i
+        #                          l
+        #                              r  
+            # ans equation = prefix[l]
+            # ans = []
+        # 
+
         
+        # nums [1,2,3,4]
+        #         i
+        # pre [1,1,2,6,24]
+        #          i
+        # # suffix [24,24,12,4,1]
+        #              i
+
+        # prefix = [1] * (len(nums)+1)
+        # for i in range(len(nums)):
+        #     prefix[i+1] = prefix[i] * nums[i]
+        
+        # suffix = [1] * (len(nums)+1)
+        # for i in reversed(range(len(nums))):
+        #     suffix[i] = suffix[i+1] * nums[i]
+
+        # ans = []
+        # for i in range(len(nums)):
+        #    answer.append(suffix[i] * prefix[i])
+
+        # return ans
+
+
         ans = [1] * len(nums)
-        
+
         prefix = 1
         for i in range(len(nums)):
-            ans[i] *= prefix
+            ans[i] = prefix
             prefix *= nums[i]
-
+        
         suffix = 1
         for i in reversed(range(len(nums))):
             ans[i] *= suffix
             suffix *= nums[i]
         return ans
+
+
+        
