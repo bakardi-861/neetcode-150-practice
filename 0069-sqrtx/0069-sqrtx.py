53a988f7bdf37f368b4f15b726f71af929661505
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-09-27 00:23:46 -0400

    Time: 2 ms (64.26%), Space: 17.7 MB (51.46%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,11 +1,17 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        l,r = 0, x+1
+        # search range from 1-x, implicitly sorted
+
+        # 1,2,3,4,5 ,6,7,8,9
+        # m
+        # l
+        #   r
+
+        l,r = 0,x+1 # +1 to include x since x could be a sqrt of itself
         while l < r:
-            mid = (r+l+1)//2
-            sqr_mid = mid ** 2
-            if sqr_mid <= x:
-                l = mid
+            mid = (r+l+1) // 2
+            if mid * mid > x:
+                r = mid - 1
             else:
-                r = mid-1
+                l = mid
         return l
