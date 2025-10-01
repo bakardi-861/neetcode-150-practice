12c12a331a3ac617404136b56c7c68d059438386
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-10-01 09:13:23 -0400

    Time: 33 ms (40.4%), Space: 20.2 MB (61.38%) - LeetHub

--- 2241-design-an-atm-machine/2241-design-an-atm-machine.py
+++ 2241-design-an-atm-machine/2241-design-an-atm-machine.py
@@ -2,15 +2,14 @@ class ATM:
     # bills: 5,20,50,100,200,500
 
     def __init__(self):
-        # MIGHT NOT NEED VALUE TO BE A LIST AND CAN JUST USE THE COUNTS
-        self.atm = {0:[20,0],1:[50,0],2:[100,0],3:[200,0],4:[500,0]} # ordered by i: $20, $50, $100, $200, and $500.
+        self.values = [20, 50, 100, 200, 500]
+        self.counts = [0] * 5
         
 
     def deposit(self, banknotesCount: List[int]) -> None:
         # $20, $50, $100, $200, and $500.
-        atm = self.atm
         for i,billCount in enumerate(banknotesCount):
-            atm[i][1] += billCount
+            self.counts[i] += billCount
 
     def withdraw(self, amount: int) -> List[int]:
         # prioritize larger bill types when withdrawing: convert to largest possible first
@@ -24,18 +23,17 @@ class ATM:
         # i = 2
 
         # iterate by largest bill
-        atm = self.atm
-        atm_backup = copy.deepcopy(self.atm)
+        counts_backup = [self.counts[i] for i in range(5)]
         atm_bills = [0] * 5
         for i in reversed(range(5)):
-            max_usable_bills = min(amount // atm[i][0], atm[i][1])
+            max_usable_bills = min(amount // self.values[i], self.counts[i])
             if max_usable_bills:
                 atm_bills[i] += max_usable_bills
-                amount -= (atm[i][0] * max_usable_bills)
-                atm[i][1] -= max_usable_bills
+                amount -= (self.values[i] * max_usable_bills)
+                self.counts[i] -= max_usable_bills
 
         if amount:
-            self.atm = atm_backup
+            self.counts = counts_backup
             return [-1]
         else:
             return atm_bills            
