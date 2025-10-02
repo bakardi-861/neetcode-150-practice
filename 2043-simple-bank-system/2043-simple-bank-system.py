6f7d4b2d5371a84359589adcc2385371362a2c3e
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-10-02 09:48:16 -0400

    Time: 32 ms (61.76%), Space: 47.6 MB (82.02%) - LeetHub

--- 2043-simple-bank-system/2043-simple-bank-system.py
+++ 2043-simple-bank-system/2043-simple-bank-system.py
@@ -1,41 +1,40 @@
 class Bank:
-    # valid transaction
-    # 1. account number is from 1 to n (len(balance))
-    # 2. the amount of money withdrawn/transferred <= account balance
+    # n counts: 1 - n
+    # balance for each account is stored in a 0-index array
+    # account "i+1" = balance[i]
 
-    # Time: O(n) for creating accounts map and O(1) for everything else
-    # Space: O(n) store the accounts
+    # Valid:
+    # 1: account numbers are between 1 - n
+    # 2: money withdrawn/transferred is <= balance[i]
 
     def __init__(self, balance: List[int]):
-        # create hashmap of accounts and insert balances as {i:balance[i]}
-        # if i already in accounts, don't insert anything
         self.balance = balance
-        self.n = len(balance)
-        #print(self.accounts)
-        
+
     def transfer(self, account1: int, account2: int, money: int) -> bool:
-        # if either account is not in accounts, return False.
-        if account1-1 > self.n or account2-1 > self.n or money > self.balance[account1-1]:
-            return False
-        
-        self.balance[account1-1] -= money
-        self.balance[account2-1] += money
-        #print(self.accounts)
-        return True
+        b = self.balance
+        # if account numbers are valid
+        if 1 <= account1 <= len(b) and 1 <= account2 <= len(b):
+            # account1 -> account2, check account1-1 balance
+            if money <= b[account1-1]:
+                b[account2-1] += money
+                b[account1-1] -= money
+                return True
+        return False
+
 
     def deposit(self, account: int, money: int) -> bool:
-        if account > self.n:
-            return False
-        self.balance[account-1] += money
-        #print(self.accounts)
-        return True
+        b = self.balance
+        if 1 <= account <= len(b):
+            b[account-1] += money
+            return True
+        return False
 
     def withdraw(self, account: int, money: int) -> bool:
-        if account > self.n or money > self.balance[account-1]:
-            return False
-        self.balance[account-1] -= money
-        #print(self.accounts)
-        return True
+        b = self.balance
+        if 1 <= account <= len(b) and money <= b[account-1]:
+            b[account-1] -= money
+            return True
+        return False
 
 
 # Your Bank object will be instantiated and called as such:
