b4dd13694b5b8977ddfba348f8056ad68cb304f0
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-10-02 20:58:08 -0400

    Time: 7 ms (82.03%), Space: 18 MB (68.22%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,20 +1,16 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # 2 ptrs
-        # "A man, a plan, a canal: Panama"
-        #                   l 
-        #                   r
-
+        # skip all non alpha characters
         l,r = 0,len(s)-1
-        while l < r:
-
-            while l < r and not s[l].isalnum():
+        while l <= r:
+            while l < len(s) and not s[l].isalnum():
                 l += 1
-            while l < r and not s[r].isalnum():
+            while r > -1 and not s[r].isalnum():
                 r -= 1
-
-            if s[l].lower() != s[r].lower():
+            
+            if l <= r and s[r].lower() != s[l].lower():
                 return False
+
             l += 1
             r -= 1
         return True
