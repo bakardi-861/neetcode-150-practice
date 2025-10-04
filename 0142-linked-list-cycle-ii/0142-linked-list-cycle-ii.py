16623fbc6fb3ff1354d91049a25c4302c8c13236
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-10-04 11:18:38 -0400

    Time: 49 ms (41.69%), Space: 19.6 MB (80.17%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -1,28 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # Initialize slow and fast pointers
         slow, fast = head,head
-
-        # Move slow one step and fast two steps
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-
-            # Check if the fast meets the slow
-            if slow == fast:
+            if fast == slow:
                 break
 
-        # Check if there is no cycle
         if not fast or not fast.next:
             return None
-
-        # Reset either slow or fast pointer to the head
+        
         fast = head
-
-        # Move both pointers one step until they meet again
         while slow != fast:
             slow = slow.next
             fast = fast.next
-
-        # Return the node where the cycle begins
         return slow
