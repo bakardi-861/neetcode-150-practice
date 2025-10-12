c6fca2c792f1c0c5d4efc756bd4d1f3c483e07dd
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-10-12 10:10:17 -0400

    Time: 3 ms (73.72%), Space: 19 MB (83.41%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -1,10 +1,16 @@
 class Solution:
-    def findClosestElements(self, nums, K, X):
-        heap = []
-        for num in nums:
-            diff = abs(X - num)
-            heappush(heap, (-diff, -num))  # store negated values for max-heap behavior
-            if len(heap) > K:
-                heappop(heap)
-        
-        return sorted([-num for _, num in heap])
+    def findClosestElements(self, nums: List[int], k: int, x: int) -> List[int]:
+        index = bisect.bisect_left(nums, x)
+        l, r = index - 1, index  # l points left, r points right
+
+        while r - l - 1 < k:  # window size = r - l - 1
+            if l < 0:
+                r += 1
+            elif r >= len(nums):
+                l -= 1
+            elif x - nums[l] <= nums[r] - x:
+                l -= 1
+            else:
+                r += 1
+
+        return nums[l+1:r]
