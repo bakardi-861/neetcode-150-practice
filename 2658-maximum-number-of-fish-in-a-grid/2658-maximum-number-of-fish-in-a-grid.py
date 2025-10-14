350c16596510a9a69249ae8b03d548887a1ad5fd
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-10-14 08:35:12 -0400

    Time: 47 ms (48.84%), Space: 18.1 MB (44.91%) - LeetHub

--- 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.py
+++ 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.py
@@ -23,6 +23,6 @@ class Solution:
         res = 0
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] != 0:
+                if grid[r][c] > 0:
                     res = max(res, dfs(r,c))
         return res
