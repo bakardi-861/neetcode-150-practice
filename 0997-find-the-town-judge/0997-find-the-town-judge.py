e928d1183722d5afee88d33bb0e147f5cec4c2b2
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-10-15 13:02:47 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -1,19 +1,28 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        if n == 1:
-            return 1
+        # judge has no outgoing edges
+        # n-1 incoming edges to the judge
+            # return immediately
+        
+        indegree = [0] * (n+1)
+        outdegree = [0] * (n+1)
+        # graph = [[] for _ in range(n+1)]
 
-        outdegree = [0] * n
-        indegree = [0] * n
-
-        # in = [0,1,1]
-        # out = [1,1,0]
+        # [1]->[2]
+     
+        # # g = [[][2][]]
+        # in = [0,0,1]
+        # out = [0,1,0]
 
         for s,d in trust:
-            indegree[d-1] += 1
-            outdegree[s-1] += 1
+            # graph[s].append(d)
+            indegree[d] += 1
+            outdegree[s] += 1
 
         for i in range(len(indegree)):
             if indegree[i] == n-1 and outdegree[i] == 0:
-                return i+1
+                return i
         return -1
+
+        
+
