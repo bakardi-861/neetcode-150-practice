d578d136a17e2df74a7eea8a8892349250a44941
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-10-16 16:33:56 -0400

    Time: 7 ms (87.36%), Space: 19.3 MB (69.51%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -7,9 +7,9 @@ class Solution:
             if left_max < right_max:
                 l += 1
                 left_max = max(left_max,height[l])
-                res += left_max - height[l] # water to the left of height[l]
+                res += left_max - height[l]
             else:
                 r -= 1
                 right_max = max(right_max,height[r])
-                res += right_max - height[r] # water to the right of height[r]
+                res += right_max - height[r]
         return res
