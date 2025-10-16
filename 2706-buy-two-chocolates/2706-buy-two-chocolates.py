08fdc0e4c77eceacfd8232ef8fc39bda5d906ebc
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-10-16 14:38:29 -0400

    Time: 3 ms (38.62%), Space: 17.9 MB (16.53%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.py
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.py
@@ -1,12 +1,38 @@
 class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
-        # greedy: smallest 2 chocolates <= money or return original money if no solution
-        # [1,2,1] money = 2
-        # [1,1] money = 1 ans = 1 (no sol)
-        # sort and return 2 pointers i and i + 1 that are less than money? -> O(nlogn)
-        n = len(prices)
-        prices.sort()
-        cost = prices[0] + prices[1]
-        if cost <= money:
-            return money - cost
+        # Array to store the frequency of prices
+        freq = [0] * 101
+        for p in prices:
+            freq[p] += 1
+        
+        # Assume minimum and second minimum to be zero.
+        # Since prices[i] cannot be 0, the 0 value implies
+        # They haven't been computed yet.
+        minimum = 0
+        second_minimum = 0
+        for price in range(1, 101):
+            if freq[price] > 1:
+                minimum = price
+                second_minimum = price
+                break
+            elif freq[price] == 1:
+                minimum = price
+                break
+        
+        # If the second minimum is not found, then find it
+        if second_minimum == 0:
+            for price in range(minimum + 1, 101):
+                if freq[price] > 0:
+                    second_minimum = price
+                    break
+        
+        # Minimum Cost
+        min_cost = minimum + second_minimum
+
+        # We can buy chocolates only if we have enough money
+        if min_cost <= money:
+            # Return the Amount of Money Left
+            return money - min_cost
+    
+        # We cannot buy chocolates. Return the initial amount of money
         return money
