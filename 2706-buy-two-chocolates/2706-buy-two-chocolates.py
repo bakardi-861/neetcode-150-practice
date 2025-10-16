726e4951e1edf9c9173f21165bac483d1bf881ed
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-10-16 14:42:54 -0400

    Time: 3 ms (38.62%), Space: 18 MB (16.53%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.py
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.py
@@ -1,38 +1,17 @@
 class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
-        # Array to store the frequency of prices
-        freq = [0] * 101
-        for p in prices:
-            freq[p] += 1
-        
-        # Assume minimum and second minimum to be zero.
-        # Since prices[i] cannot be 0, the 0 value implies
-        # They haven't been computed yet.
-        minimum = 0
-        second_minimum = 0
-        for price in range(1, 101):
-            if freq[price] > 1:
-                minimum = price
-                second_minimum = price
-                break
-            elif freq[price] == 1:
-                minimum = price
-                break
-        
-        # If the second minimum is not found, then find it
-        if second_minimum == 0:
-            for price in range(minimum + 1, 101):
-                if freq[price] > 0:
-                    second_minimum = price
-                    break
-        
-        # Minimum Cost
-        min_cost = minimum + second_minimum
+        minimum = min(prices[0],prices[1]) # find min of first two
+        second = max(prices[0],prices[1]) # second is the second min, so choose the other one of the first two
 
-        # We can buy chocolates only if we have enough money
+        for i in range(2,len(prices)):
+            if prices[i] < minimum:
+                second = minimum
+                minimum = prices[i]
+            elif prices[i] < second:
+                second = prices[i]
+
+        min_cost = minimum + second
+
         if min_cost <= money:
-            # Return the Amount of Money Left
             return money - min_cost
-    
-        # We cannot buy chocolates. Return the initial amount of money
         return money
