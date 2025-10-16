71e58198f26e2f5c854cdd744834ef5587f9412b
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-10-16 15:02:23 -0400

    Time: 0 ms (100%), Space: 17.9 MB (38.73%) - LeetHub

---
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
@@ -0,0 +1,14 @@
+class Solution:
+    def areAlmostEqual(self, s1: str, s2: str) -> bool:
+        if s1 == s2:
+            return True
+
+        diff = [(a, b) for a, b in zip(s1, s2) if a != b]
+
+        # Must have exactly 2 mismatches and they must "cross match"
+        return len(diff) == 2 and diff[0] == diff[1][::-1]
+
+
+        
+
+
