941b805d3d2c17829fe469e24871b4cf10faf745
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-10-16 15:29:34 -0400

    Time: 28 ms (39.09%), Space: 23.4 MB (54.94%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,16 +1,13 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        prefix = [1] * n  # prefix[i] will hold product of elements to the left of i
-
-        # Step 1: build prefix array without a separate run variable
-        for i in range(1, n):
-            prefix[i] = prefix[i-1] * nums[i-1]
-
-        # Step 2: multiply in suffix products using a running variable
-        suffix = 1
-        for i in reversed(range(n)):
-            prefix[i] *= suffix
-            suffix *= nums[i]
-
-        return prefix
+        ans = [1] * len(nums)
+       
+        left, right = 1, 1
+        for i in range(len(nums)):
+            ans[i] *= left
+            ans[~i] *= right
+            
+            left *= nums[i]
+            right *= nums[~i]
+            
+        return ans
