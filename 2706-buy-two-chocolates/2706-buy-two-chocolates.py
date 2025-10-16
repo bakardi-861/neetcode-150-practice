6e663b85263eff3808a044e344f23f01bcc5559f
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-10-16 14:35:29 -0400

    Time: 4 ms (20.05%), Space: 18 MB (16.53%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.py
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.py
@@ -1,13 +1,12 @@
 class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
-        # greedy: smallest 2 chocolates < money or return original money if no solution
+        # greedy: smallest 2 chocolates <= money or return original money if no solution
         # [1,2,1] money = 2
         # [1,1] money = 1 ans = 1 (no sol)
         # sort and return 2 pointers i and i + 1 that are less than money? -> O(nlogn)
         n = len(prices)
         prices.sort()
-        for i in range(n-1):
-            cost = prices[i] + prices[i+1]
-            if i < n and cost <= money:
-                return money - cost
+        cost = prices[0] + prices[1]
+        if cost <= money:
+            return money - cost
         return money
