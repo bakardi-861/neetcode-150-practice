cc2db966bfd4c9c79918de15c4186a22ead82aa8
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-10-16 15:50:34 -0400

    Time: 0 ms (100%), Space: 17.8 MB (43.93%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,19 +1,24 @@
 class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
-        # row set, col set, box set
-        row_set = [set() for _ in range(9)]
-        col_set = [set() for _ in range(9)]
-        box_set = [set() for _ in range(9)]
-        for r in range(9):
-            for c in range(9):
-                num = board[r][c]
-                if num == ".": continue
-                box_index = (r//3) * 3 + (c//3)
+    def isValidSudoku(self, grid: List[List[str]]) -> bool:
+        # "." = not filled
+        # each row must be a set of 1-9
+        # each col must be a set of 1-9
+        # each 3x3 box must be a set 1-9: get indices with r/3, c/3
+        rows,cols = len(grid),len(grid[0])
+        boxes = [set() for _ in range(10)]
+        col_set = [set() for _ in range(10)]
+        row_set = [set() for _ in range(10)]
 
-                if num in row_set[r] or num in col_set[c] or num in box_set[box_index]:
+        for r in range(rows):
+            for c in range(cols):
+                curr = grid[r][c]
+                if curr == ".":
+                    continue
+
+                if curr in row_set[r] or curr in col_set[c] or curr in boxes[3*(r//3) + (c//3)]:
                     return False
 
-                row_set[r].add(num)
-                col_set[c].add(num)
-                box_set[box_index].add(num)
+                row_set[r].add(curr)
+                col_set[c].add(curr)
+                boxes[3*(r//3) + (c//3)].add(curr)
         return True
