58801b44b84d95e908175f55faa8ed0d5eae4716
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-10-17 09:42:47 -0400

    Time: 0 ms (100%), Space: 17.7 MB (81.92%) - LeetHub

--- 0156-binary-tree-upside-down/0156-binary-tree-upside-down.py
+++ 0156-binary-tree-upside-down/0156-binary-tree-upside-down.py
@@ -6,24 +6,9 @@
 #         self.right = right
 class Solution:
     def upsideDownBinaryTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        # og left child becomes new root
-        # og root becomes right child
-        # og right child becomes left
-        # everything is kind of shifted in a trinagle pattern
-        # level by level - BFS
-            # tree size is only 10, so could do backtracking?
-        # every right node has a sibling (left node with same parent) and no children. 
-            # og right children are always leaves
-
-        if not root: return None
-        curr = root
-        prev,next,old_right = None,None,None
-        while curr:
-            next = curr.left
-            curr.left = old_right
-            old_right = curr.right
-            curr.right = prev
-            prev = curr
-            curr = next
-        return prev
-
+        if not root:
+            return None
+        l,r,root.left,root.right = root.left, root.right,None,None
+        while l:
+            l.left,l.right, root,l,r = r,root,l,l.left,l.right
+        return root
