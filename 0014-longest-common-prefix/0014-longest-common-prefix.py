e7ac91a2212e1b1ad998ed040e87ea13ac5dc91e
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-10-24 00:14:21 +0000

    Time: 0 ms (100%), Space: 17.7 MB (95.81%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,66 +1,20 @@
-class TrieNode:
-    def __init__(self):
-        self.nodes = {}  # map char to TrieNode
-        self.is_leaf = False
-
-    def insert_many(self, words: list[str]):
-        for word in words:
-            self.insert(word)
-
-    def insert(self, word: str):
-        curr = self
-        for char in word:
-            if char not in curr.nodes:
-                curr.nodes[char] = TrieNode()
-            curr = curr.nodes[char]
-        curr.is_leaf = True
-
-    def find(self, word: str) -> bool:
-        curr = self
-        for char in word:
-            if char not in curr.nodes:
-                return False
-            curr = curr.nodes[char]
-        return curr.is_leaf
-
-    def delete(self, word: str):
-        def _delete(curr: 'TrieNode', word: str, i: int):
-            # base case
-            if i == len(word):  # fixed = to ==
-                if not curr.is_leaf:
-                    return False
-                curr.is_leaf = False
-                return len(curr.nodes) == 0
-
-            char = word[i]
-            char_node = curr.nodes.get(char)
-            if not char_node:
-                return False
-
-            delete_curr = _delete(char_node, word, i + 1)
-            if delete_curr:
-                del curr.nodes[char]
-                return len(curr.nodes) == 0
-
-            return False
-
-        _delete(self, word, 0)
-
-
 class Solution:
-    def longestCommonPrefix(self, strs: list[str]) -> str:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
         if not strs:
             return ""
+        minLen = min(len(x) for x in strs)
+        low, high = 1, minLen
+        while low <= high:
+            middle = (low + high) // 2
+            if self.isCommonPrefix(strs, middle):
+                low = middle + 1
+            else:
+                high = middle - 1
+        return strs[0][: (low + high) // 2]
 
-        trie = TrieNode()
-        trie.insert_many(strs)
-
-        # traverse down the trie while only one branch and not a leaf
-        prefix = ""
-        curr = trie
-        while len(curr.nodes) == 1 and not curr.is_leaf:
-            char = next(iter(curr.nodes))
-            prefix += char
-            curr = curr.nodes[char]
-
-        return prefix
+    def isCommonPrefix(self, strs, l):
+        str1 = strs[0][:l]
+        for i in range(1, len(strs)):
+            if not strs[i].startswith(str1):
+                return False
+        return True
