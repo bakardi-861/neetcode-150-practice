77dfb43db4bebae20f2e420b8bbdfaa806747e75
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-10-24 14:55:15 +0000

    Time: 61 ms (41.1%), Space: 23.1 MB (43.14%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.py
+++ 0705-design-hashset/0705-design-hashset.py
@@ -1,24 +1,78 @@
-class MyHashSet:
+
+class MyHashSet(object):
 
     def __init__(self):
-        self.buckets = [[] for _ in range(10 ** 4)]
+        """
+        Initialize your data structure here.
+        """
+        self.keyRange = 769
+        self.bucketArray = [Bucket() for i in range(self.keyRange)]
 
-    def hash(self,key):
-        return key % len(self.buckets)
-        
-    def add(self, key: int) -> None:
-        i = self.hash(key)
-        if key not in self.buckets[i]:
-            self.buckets[i].append(key)
-    
-    def remove(self, key: int) -> None:
-        i = self.hash(key)
-        if key in self.buckets[i]:
-            self.buckets[i].remove(key)
-        
-    def contains(self, key: int) -> bool:
-         i = self.hash(key)
-         return key in self.buckets[i]
+    def _hash(self, key):
+        return key % self.keyRange
+
+    def add(self, key):
+        """
+        :type key: int
+        :rtype: None
+        """
+        bucketIndex = self._hash(key)
+        self.bucketArray[bucketIndex].insert(key)
+
+    def remove(self, key):
+        """
+        :type key: int
+        :rtype: None
+        """
+        bucketIndex = self._hash(key)
+        self.bucketArray[bucketIndex].delete(key)
+
+    def contains(self, key):
+        """
+        Returns true if this set contains the specified element
+        :type key: int
+        :rtype: bool
+        """
+        bucketIndex = self._hash(key)
+        return self.bucketArray[bucketIndex].exists(key)
+
+
+class Node:
+    def __init__(self, value, nextNode=None):
+        self.value = value
+        self.next = nextNode
+
+class Bucket:
+    def __init__(self):
+        # a pseudo head
+        self.head = Node(0)
+
+    def insert(self, newValue):
+        # if not existed, add the new element to the head.
+        if not self.exists(newValue):
+            newNode = Node(newValue, self.head.next)
+            # set the new head.
+            self.head.next = newNode
+
+    def delete(self, value):
+        prev = self.head
+        curr = self.head.next
+        while curr is not None:
+            if curr.value == value:
+                # remove the current node
+                prev.next = curr.next
+                return
+            prev = curr
+            curr = curr.next
+
+    def exists(self, value):
+        curr = self.head.next
+        while curr is not None:
+            if curr.value == value:
+                # value existed already, do nothing
+                return True
+            curr = curr.next
+        return False
 
 
 # Your MyHashSet object will be instantiated and called as such:
