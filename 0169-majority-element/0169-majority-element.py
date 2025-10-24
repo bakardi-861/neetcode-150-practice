b3036abf4698296012e9f79d005127c1ce14a3ca
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-10-24 10:54:57 +0000

    Time: 8 ms (37.54%), Space: 19.5 MB (5.17%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,4 +1,23 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count = Counter(nums)
-        return max(count.keys(),key=count.get)
+        # naive: use hashmap to count how many times each element has appeared and then iterate through it to find the one that appears more than n/2 times.
+        # O(n) time and O(n) space in the worst case
+        # To get O(1) space, I have to use an integer counter variable and not use a hashmap
+        # The problem is when I see a different element, how can I keep track of the counts without storing them in something. Example, I count two 2s and then the next number is a 1, the count will get reset.
+
+        # I know that Boyer voting algorithm is an option, but I just know the name and not how to implement it unfortunately. In lieu of that, I can use a variable majority to track the element with the highest frequency we have seen so far, maybe as a tuple with (num,count). 
+        # [2,2,1,1,1,2,2]
+        #              i
+        # majority = (2,4)
+        # res = 2
+
+        map = Counter(nums)
+        n = len(nums)
+        for key,val in map.items():
+            if val >= (n/2):
+                return key
+                
+
+
+
+ 
