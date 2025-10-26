7d0a42c55f13c827b5416cae4b3ad098eb4b9601
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-10-26 00:33:31 +0000

    Time: 0 ms (100%), Space: 17.7 MB (60.15%) - LeetHub

---
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
@@ -0,0 +1,12 @@
+class Solution:
+    def totalMoney(self, n):
+        total = 0
+        complete_week = n // 7
+        remaining_days = n % 7
+
+        # total hamming distance
+        total += (28 * complete_week) + (7 * (complete_week - 1) * complete_week // 2) #total for complete weeks
+
+        total += (complete_week + 1 + complete_week + remaining_days) * remaining_days // 2 #total for remaining days
+
+        return total
