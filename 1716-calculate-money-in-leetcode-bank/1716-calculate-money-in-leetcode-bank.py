e50974b00be506b8ddaaa099715b74615afadd43
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-10-26 00:42:52 +0000

    Time: 0 ms (100%), Space: 17.9 MB (15.71%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
@@ -1,14 +1,15 @@
 class Solution:
-    def totalMoney(self, n):
-        total = 0
-        complete_week = n // 7
-        remaining_days = n % 7
+    def totalMoney(self, n: int) -> int:
+        k = n // 7
+        F = 28 # first num in sequence (known sum of full week)
+        L = 28 + (k - 1) * 7 # last num in sequence (known sum of full week + full weeks-1 * 7)
+        arithmetic_sum = k * (F + L) // 2 # sum of full weeks
+        
+        monday = 1 + k # increment 1 by how many additional full weeks since first week always starts at 1
+        final_week = 0
 
-        # total hamming distance
-        total += (28 * complete_week) + (7 * (complete_week - 1) * complete_week // 2) #total for complete weeks
-
-        total += (complete_week + 1 + complete_week + remaining_days) * remaining_days // 2 #total for remaining days
-
-        return total
-
-        # can also simulate with a while loop/for loop in O(n) time
+        # leftover days - this can also be an arithmetic sequence, but it is still O(1)
+        for day in range(n % 7):
+            final_week += monday + day
+        
+        return arithmetic_sum + final_week
