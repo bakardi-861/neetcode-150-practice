fb439f9040af51c886518621c3819ef4a905dba9
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-10-27 13:10:15 +0000

    Time: 0 ms (100%), Space: 17.7 MB (83.62%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -1,23 +1,12 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        # stack - Meta-tagged
-        # directories separated by single '/', treat multiple slashes as 1
-        # delete trailing '/' unless it is root directory
-        # anything not '.','..' or '/' treated as a directory name
-
-        # Input: path = "/home/user/Documents/../Pictures"
-        #                                                 i
-        # ['/','home','/','user','/','Pictures']
         split_path = path.split('/')
         stack = []
         
         for c in split_path:
-            if c not in ['','.','..']: 
+            if c == '..':
+                if stack:
+                    stack.pop()
+            elif c and c != '.': 
                 stack.append(c)
-            elif stack and c == '..':
-                stack.pop()
         return '/' + "/".join(stack)
-
-
-
-
