8c59fe2af52b01c8b2b5a6fb8328d2bc642c9ff8
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-10-27 21:45:54 +0000

    Time: 502 ms (20.98%), Space: 39.7 MB (14.62%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,22 +1,35 @@
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
-        max_q,min_q = deque(),deque()
-        l = 0
-        longest = 0
-        for r,num in enumerate(nums):
-            #while to handle duplicates
-            while max_q and num > max_q[-1]:
-                max_q.pop()
-            max_q.append(num)
-            while min_q and num < min_q[-1]:
-                min_q.pop()
-            min_q.append(num)
+        # longest subarray = sliding window
+        # limit = k
+        # absolute diff = max abs diff
+        # nums = [10,1,1,1,7,2], limit = 5
+        #            l
+        #                r
+        # r = 5
+        # window = {10:1} 
+        # max_num = 7 # how would i do when shifting left?
+        # min_num = 2
+        # max_abs_diff = abs(7-2) = 5
+        # limit = 5
+        # longest = 4
 
-            while max_q[0] - min_q[0] > limit:
-                if nums[l] == min_q[0]:
-                    min_q.popleft() 
-                if nums[l] == max_q[0]:
-                    max_q.popleft()
-                l += 1
-            longest = max(r-l+1,longest)
+        longest = 0
+        l = 0 
+        min_heap = []
+        max_heap = []
+
+        for r in range(len(nums)):
+            heappush(min_heap,(nums[r],r)) # O(logn)
+            heappush(max_heap,(-nums[r],r))
+
+            # move l 
+            while -max_heap[0][0] - min_heap[0][0] > limit:
+                l = min(max_heap[0][1],min_heap[0][1]) + 1
+                while max_heap[0][1] < l:
+                    heappop(max_heap)
+                while min_heap[0][1] < l:
+                    heappop(min_heap)
+
+            longest = max(longest, r-l+1)
         return longest
