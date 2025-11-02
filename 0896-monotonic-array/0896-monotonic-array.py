7761fb3d57361754c448835017c57c53dbf34760
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-11-02 16:50:56 +0000

    Time: 26 ms (69.98%), Space: 29 MB (70.04%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.py
+++ 0896-monotonic-array/0896-monotonic-array.py
@@ -1,11 +1,13 @@
 class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
         mono_de = mono_in = True
-        st = []
+        last = None
+        # st = []
         for n in nums:
-            if st and n > st[-1]:
+            if last is not None and n > last:
                 mono_de = False
-            if st and n < st[-1]:
+            if last is not None and n < last:
                 mono_in = False
-            st.append(n)
+            last = n
+            # st.append(n)
         return mono_de or mono_in
