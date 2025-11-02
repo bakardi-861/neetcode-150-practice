96540cf2eaf7bd9c9697bbefd6d8b13fea4793a6
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-11-02 21:32:31 +0000

    Time: 43 ms (21.02%), Space: 17.9 MB (32.21%) - LeetHub

--- 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.py
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.py
@@ -1,6 +1,5 @@
 class Solution:
     def countOperations(self, num1: int, num2: int) -> int:
-        # operation = if num1 >= num2, subtract num2 form num1. else subtract num1 from num2
         ops = 0
         while num1 > 0 and num2 > 0:
             if num1 >= num2:
