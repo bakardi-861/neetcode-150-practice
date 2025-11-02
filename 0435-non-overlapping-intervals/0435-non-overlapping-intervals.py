5a90b67a281979aa5aa798852362b1685e09e25b
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-11-02 20:01:58 +0000

    Time: 80 ms (63.77%), Space: 51.4 MB (56.02%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -3,11 +3,11 @@ class Solution:
         # if i would have to merge an interval bc start = start, delete the one I would merge
         # end == start is ok
         intervals.sort(key=lambda x: x[1]) # this only seems to work when sorted by end time
-        last = []
+        last = float("-inf")
         count = 0
         for s,e in intervals:
-            if not last or last[1] <= s:
-                last = [s,e]
+            if last <= s:
+                last = e
             else:
                 count += 1
         return count
