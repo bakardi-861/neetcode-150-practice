1116a57472e39123ce2ec5fc4a21fd6a804e12f4
Jesse <58563075+bakardi-861@users.noreply.github.com>
2025-11-02 18:24:05 +0000

    Time: 7 ms (72.94%), Space: 21.7 MB (32.51%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,14 +1,10 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        # write pseudocode, then change to correct code
+        intervals.sort(key=lambda x: x[0])
         merged = []
-        intervals.sort(key=lambda x:x[0])
-
-        for interval in intervals:
-            # if merged array is empty or last merged end time is smaller than current interval start time
-            if not merged or merged[-1][1] < interval[0]:
-                merged.append(interval)
-            else: # current interval is smaller than last merged, so that means we have to merge both
-                # merge current interval into last merged interval
-                merged[-1] = [min(interval[0],merged[-1][0]),max(interval[1],merged[-1][1])]
+        for s,e in intervals:
+            if not merged or merged[-1][1] < s:
+                merged.append([s,e])
+            else:
+                merged[-1][1] = max(merged[-1][1],e)
         return merged
